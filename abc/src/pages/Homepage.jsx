1e57558c7c7605f35a357450d98750689434f474
Muhammad Aqib <aqibashraf397@gmail.com>
2025-11-14 17:51:19 +0500

    img size 500px

--- abc/src/pages/Homepage.jsx
+++ abc/src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ function Homepage() {
   return (
     <>
     
-      <img style={{width:200}} src="/images/cvpic.jpg" alt="mypic" />
+      <img style={{width:500}} src="/images/cvpic.jpg" alt="mypic" />
 
     </>
   );
